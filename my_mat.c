5e8e475b8950bb4c89fdc7e5be9d6ca5acd5913f
Hagai Hen <76903853+HagaiHen@users.noreply.github.com>
2021-11-23 17:28:33 +0200

    Create my_mat.c

--- my_mat.c
+++ my_mat.c
@@ -3,16 +3,15 @@
 
 static int arr [10][10];
 
-int getMat() { //get the values from the user
+void getMat() { //get the values from the user
     for (int i =0 ; i<10; i++) {
         for (int j =0; j<10; j++) {
             scanf("%d ", &arr[i][j]);
         }
     }
-    return 0;
 }
 
-int PathExist () { //this function check if these a path between 2 points. returns True or False.
+void PathExist () { //this function check if these a path between 2 points. returns True or False.
     int n = 0, m = 0;
     scanf("%d", &n);
     scanf("%d", &m);
@@ -33,14 +32,12 @@ int PathExist () { //this function check if these a path between 2 points. retur
 
     if (arr[n][m] > 0) {
         printf("True\n");
-        return 1;
     } else {
         printf("False\n");
-        return 0;
     }
 }
 //This function find the shortest path between 2 points
-int ShortestPath () {
+void ShortestPath () {
     int n = 0, m = 0;
     scanf("%d", &n);
     scanf("%d", &m);
@@ -58,11 +55,9 @@ int ShortestPath () {
     }
     if (arr[n][m] == 0) {
             printf("-1\n");
-            return -1;
         }
         else {
             printf("%d\n", arr[n][m]);
-            return arr[n][m];
         }
     
 }
