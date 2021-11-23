377e9491aea637eba26ffb857df2323452e4d55d
Hagai Hen <76903853+HagaiHen@users.noreply.github.com>
2021-11-23 17:25:36 +0200

    Update my_mat.c

--- my_mat.c
+++ my_mat.c
@@ -3,41 +3,28 @@
 
 static int arr [10][10];
 
-int getMat() {
+int getMat() { //get the values from the user
     for (int i =0 ; i<10; i++) {
         for (int j =0; j<10; j++) {
             scanf("%d ", &arr[i][j]);
         }
     }
-
-    for(int k=0; k<10; k++){
-        for(int i=0; i<10; i++){
-            for (int j=0; j<10; j++){
-                if (arr[i][j] == 0 && arr[i][k] != 0 && arr[k][j] != 0) {
-                    arr[i][j] = arr[i][k] + arr[k][j];
-                }
-                if (arr[i][k] + arr[k][j] < arr[i][j] && arr[i][k] != 0 && arr[k][j] != 0) {
-                    arr[i][j] = arr[i][k] + arr[k][j];
-                }
-            }
-        }
-    }
-    
     return 0;
 }
 
-int PathExist () {
+int PathExist () { //this function check if these a path between 2 points. returns True or False.
     int n = 0, m = 0;
     scanf("%d", &n);
     scanf("%d", &m);
-    for(int k=0; k<10; k++){
+    // algorithm to find the shortest path.
+    for(int k=0; k<10; k++) {
         for(int i=0; i<10; i++){
             for (int j=0; j<10; j++){
                 
                 if (arr[i][k] + arr[k][j] < arr[i][j] && arr[i][k] != 0 && arr[k][j] != 0 && arr[i][j] != 0) {
                     arr[i][j] = arr[i][k] + arr[k][j];
                 }
-                if (i == j) {
+                if (i == j) { //set zeros in the diagonal (the distance between point to itselt is zero.
                     arr[i][j] = 0;
                 }
             }
@@ -46,12 +33,13 @@ int PathExist () {
 
     if (arr[n][m] > 0) {
         printf("True\n");
+        return 1;
     } else {
         printf("False\n");
+        return 0;
     }
-    return 0;
 }
-
+//This function find the shortest path between 2 points
 int ShortestPath () {
     int n = 0, m = 0;
     scanf("%d", &n);
@@ -62,7 +50,7 @@ int ShortestPath () {
                 if (arr[i][k] + arr[k][j] < arr[i][j] && arr[i][k] != 0 && arr[k][j] != 0 && arr[i][j] != 0) {
                     arr[i][j] = arr[i][k] + arr[k][j];
                 }
-                if (i == j) {
+                if (i == j) { //set zeros in the diagonal (the distance between point to itselt is zero.
                     arr[i][j] = 0;
                 }
             }
@@ -70,9 +58,11 @@ int ShortestPath () {
     }
     if (arr[n][m] == 0) {
             printf("-1\n");
+            return -1;
         }
         else {
             printf("%d\n", arr[n][m]);
+            return arr[n][m];
         }
-    return 0;
+    
 }
