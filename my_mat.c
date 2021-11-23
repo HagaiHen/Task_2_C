6f022240ee4b39bbbffa20fb0802517b2a20c296
Hagai Hen <76903853+HagaiHen@users.noreply.github.com>
2021-11-23 17:57:33 +0200

    Update my_mat.c

--- my_mat.c
+++ my_mat.c
@@ -10,26 +10,6 @@ int getMat() { //get the values from the user
         }
     }
 
-    for(int k=0; k<10; k++){
-        for(int i=0; i<10; i++){
-            for (int j=0; j<10; j++){
-                if (arr[i][j] == 0 && arr[i][k] != 0 && arr[k][j] != 0) {
-                    arr[i][j] = arr[i][k] + arr[k][j];
-                }
-                if (arr[i][k] + arr[k][j] < arr[i][j] && arr[i][k] != 0 && arr[k][j] != 0) {
-                    arr[i][j] = arr[i][k] + arr[k][j];
-                }
-            }
-        }
-    }
-    
-    return 0;
-}
-
-int PathExist () {  // this function check if these a path between 2 points. prints True or False.
-    int n = 0, m = 0;
-    scanf("%d", &n);
-    scanf("%d", &m);
     // algorithm to find the shortest path.
     for(int k=0; k<10; k++){
         for(int i=0; i<10; i++){
@@ -44,7 +24,15 @@ int PathExist () {  // this function check if these a path between 2 points. pri
             }
         }
     }
+    
+    return 0;
+}
 
+int PathExist () {  // this function check if these a path between 2 points. prints True or False.
+    int n = 0, m = 0;
+    scanf("%d", &n);
+    scanf("%d", &m);
+    
     if (arr[n][m] > 0) {
         printf("True\n");
     } else {
@@ -57,18 +45,7 @@ int ShortestPath () { //This function find the shortest path between 2 points
     int n = 0, m = 0;
     scanf("%d", &n);
     scanf("%d", &m);
-    for(int k=0; k<10; k++){
-        for(int i=0; i<10; i++){
-            for (int j=0; j<10; j++){
-                if (arr[i][k] + arr[k][j] < arr[i][j] && arr[i][k] != 0 && arr[k][j] != 0 && arr[i][j] != 0) {
-                    arr[i][j] = arr[i][k] + arr[k][j];
-                }
-                if (i == j) { //set zeros in the diagonal (the distance between point to itselt is zero.
-                    arr[i][j] = 0;
-                }
-            }
-        }
-    }
+    
     if (arr[n][m] == 0) {
             printf("-1\n");
         }
